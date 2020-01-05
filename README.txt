4e902f69d6e84a9d5142371877a0dc30aa35278c
Przemysław Marszałek <przem.mar.15@gmail.com>
2020-01-05 17:36:36 +0100

    add communications with db

--- README.txt
+++
@@ -1,1 +0,0 @@
-Example 3 commit on testDrive feature
---
+++ Services/KeyGeneratorService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace PharmacyWebApp.Services
+{
+    public class KeyGeneratorService
+    {
+
+        public string GenerateKey(int size)
+        {
+            char[] chars =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".ToCharArray();
+            byte[] data = new byte[size];
+            using (RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider())
+            {
+                crypto.GetBytes(data);
+            }
+            StringBuilder result = new StringBuilder(size);
+            foreach (byte b in data)
+            {
+                result.Append(chars[b % (chars.Length)]);
+            }
+            return result.ToString();
+        }
+
+    }
+}
