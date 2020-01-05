ce667ab7e04a6c4913e9c513cb9c829c21931abd
Wojto <wlacheta@interia.pl>
2020-01-05 16:58:29 +0100

    Home ViewModel added

---
+++ Models/ViewModels/ContainerViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PharmacyWebApp.Models
+{
+    public class ContainerViewModel
+    {
+        public string Message { get; set; }
+
+        public Size MedicineSize { get; set; }
+
+        [Display(Name = "Klucz apteki")]
+        [Required(ErrorMessage = "Uzupełnij pole")]
+        public string PharmacyKey { get; set; }
+
+        [Display(Name = "Lek")]
+        public List<Medicine> Medicines { get; set; } = new List<Medicine>();
+
+        public List<SelectListItem> Units { get; set; }
+
+    }
+}
